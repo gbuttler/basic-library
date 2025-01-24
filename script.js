e98534e2bc9547ede883d78f783f83ae36048041
Grace Buttler <grace.buttler@gmail.com>
2025-01-24 19:17:15 +0000

    work out how to get the book information to display. Success!

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 const myLibrary = [];
 
 // Book object
-function Book(title, author, numpages, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.numpages = numpages;
+  this.pages = pages;
   this.read = read;
 }
 
 // function that will add a new book to the library
-function addBookToLibrary(title, author, numpages, read) {
-  const newBook = new Book(title, author, numpages, read);
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
 
   myLibrary.push(newBook);
 }
@@ -24,14 +24,52 @@ addBookToLibrary("Blah", "A. Blaj", "3 pages", "Read");
 addBookToLibrary("blegh ", "blegh", "139000 pages", "Not read");
 
 // create function to display book on HTML page
-let library = document.getElementsByClassName("library");
+const library = document.querySelector(".library");
+let booksToDisplay = "";
 console.log(library);
 myLibrary.forEach((Book) => {
+  //check that the book info is being recieved
   console.log(Book.title);
-  //   let libraryBook = document.createElement("div");
-  //   console.log(libraryBook);
-  //   libraryBook.setAttribute("class", "library-book");
-  //   library.append(Book.title);
+  // append to the HTML
+  booksToDisplay += `
+  <div class="library-book">
+        <p class="book-title">${Book.title}</p>
+        <p class="book-author">${Book.author}</p>
+        <p class="book-pages">${Book.pages}</p>
+        <p class="book-isRead">${Book.read}</p>
+      </div>
+  `;
+
+  library.innerHTML = booksToDisplay;
+
+  console.log(booksToDisplay);
+
+  //create all of the elements to house the information
+  // let libraryBook = document.createElement("div");
+  // libraryBook.setAttribute("class", "library-book");
+  // let bookTitle = document.createElement("p");
+  // bookTitle.setAttribute("class", "book-title");
+  // let bookAuthor = document.createElement("p");
+  // bookAuthor.setAttribute("class", "book-author");
+  // let bookPages = document.createElement("p");
+  // bookPages.setAttribute("class", "book-pages");
+  // let bookIsRead = document.createElement("p");
+  // bookIsRead.setAttribute("class", "book-isRead");
+
+  // append the paragraphs to the div
+  // libraryBook.appendChild(bookTitle);
+  // libraryBook.appendChild(bookAuthor);
+  // libraryBook.appendChild(bookPages);
+  // libraryBook.appendChild(bookIsRead);
+
+  // console.log(libraryBook);
+
+  // get and set attributes from books in array
+  // bookTitle.innerHTML = Book.title;
+  // bookAuthor.innerHTML = Book.author;
+  // bookPages.innerHTML = Book.pages;
+  // bookIsRead.innerHTML = Book.read;
+  // libraryBook.appendChild(Book.title);
   // create p for title and set attribute
   // create p for author and set attribute
   // create p for pages and set attribute
