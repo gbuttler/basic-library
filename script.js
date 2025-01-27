8b926eccb82f89fa46eb2c4badafe649c4b54ffe
Grace Buttler <grace.buttler@gmail.com>
2025-01-27 12:38:46 +0000

    allow new book to be added to the display

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// an array to hold the books - this will later be looped through to display the books
+// an array to hold the books
 const myLibrary = [];
+// finding the library class on the HTML page
+const library = document.querySelector(".library");
 
 // Book object
 function Book(title, author, pages, read) {
@@ -16,35 +18,43 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
-// dummy new book for test purposes
+// dummy new books for test purposes
 addBookToLibrary("Turning Pages", "A. Round", "139 pages", "Read");
 
 addBookToLibrary("Blah", "A. Blaj", "3 pages", "Read");
 
 addBookToLibrary("blegh ", "blegh", "139000 pages", "Not read");
 
-// create function to display book on HTML page
-const library = document.querySelector(".library");
-let booksToDisplay = "";
-console.log(library);
-myLibrary.forEach((Book) => {
-  //check that the book info is being recieved
-  console.log(Book.title);
-  // append to the HTML
-  booksToDisplay += `
+// function to display book on HTML page
+function displayLibrary() {
+  //set book display to empty
+  let booksToDisplay = "";
+  myLibrary.forEach((Book) => {
+    //check that the book info is being recieved
+    console.log(Book.title);
+
+    // append to the HTML
+    booksToDisplay += `
   <div class="library-book">
         <p class="book-title">${Book.title}</p>
         <p class="book-author">${Book.author}</p>
         <p class="book-pages">${Book.pages}</p>
         <p class="book-isRead">${Book.read}</p>
+        <input type="checkbox" id="read-checkbox">
+        <p class='book-read-status' id="book-read" style="display:none">Read</p>
+        <p class='book-read-status' id="book-not-read" style="display:none">Not read</p>
+        <button class=delete-book-button>Delete Book</button>
       </div>
   `;
 
-  library.innerHTML = booksToDisplay;
+    library.innerHTML = booksToDisplay;
+  });
+}
 
-  console.log(booksToDisplay);
-});
+//call display library function
+displayLibrary();
 
+//function to display new book form
 function showForm() {
   //check this function is being accessed
   console.log("show form button clicked");
@@ -53,6 +63,7 @@ function showForm() {
   libraryForm.classList.toggle("hidden");
 }
 
+//function to take form information and display on new book card
 document
   .getElementById("library-form")
   .addEventListener("submit", function (event) {
@@ -63,62 +74,25 @@ document
     Book.title = event.target.title.value;
     Book.author = event.target.author.value;
     Book.pages = event.target.pages.value;
-    Book.read - event.target.read.value;
+    Book.read = event.target.read.value;
 
-    function addBookToLibrary(title, author, pages, read) {
-      const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(Book.title, Book.author, Book.pages, Book.read);
+    displayLibrary();
 
-      myLibrary.push(newBook);
-    }
-
-    addBookToLibrary();
     console.log(myLibrary);
   });
 
-// idea section below
+//function to check if book is read
 
-//use to add array items to page? - code copied from a previous project
-// document
-//   .getElementsByClassName("library")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     console.log("I am submitted");
-//     const bookTitle = document.getElementsByClassName("book-title");
-//     para = document.createElement("p");
-//     para.setAttribute("class", "projectSummary");
-//     projectDesc.appendChild(para);
-//     projectObject.projectName = event.target.name.value;
-//     projectObject.projectMedium = event.target.medium.value;
-//     para.innerHTML = `My project is called ${projectObject.projectName} and I'm going to be ${projectObject.projectMedium} it`;
-//   });
+function checkIfRead() {
+  let readCheckbox = document.getElementById("read-checkbox");
+  let yesRead = document.getElementById("book-read");
+  let notRead = document.getElementById("book-not-read");
 
-//create all of the elements to house the information
-// let libraryBook = document.createElement("div");
-// libraryBook.setAttribute("class", "library-book");
-// let bookTitle = document.createElement("p");
-// bookTitle.setAttribute("class", "book-title");
-// let bookAuthor = document.createElement("p");
-// bookAuthor.setAttribute("class", "book-author");
-// let bookPages = document.createElement("p");
-// bookPages.setAttribute("class", "book-pages");
-// let bookIsRead = document.createElement("p");
-// bookIsRead.setAttribute("class", "book-isRead");
-
-// append the paragraphs to the div
-// libraryBook.appendChild(bookTitle);
-// libraryBook.appendChild(bookAuthor);
-// libraryBook.appendChild(bookPages);
-// libraryBook.appendChild(bookIsRead);
-
-// console.log(libraryBook);
-
-// get and set attributes from books in array
-// bookTitle.innerHTML = Book.title;
-// bookAuthor.innerHTML = Book.author;
-// bookPages.innerHTML = Book.pages;
-// bookIsRead.innerHTML = Book.read;
-// libraryBook.appendChild(Book.title);
-// create p for title and set attribute
-// create p for author and set attribute
-// create p for pages and set attribute
-// create p for read or not and set attribute
+  //tick box if book is marked as read in the object
+  if (Book.read === "Read") {
+    yesRead.style.display = "block";
+  } else {
+    notRead.style.display = "block";
+  }
+}
